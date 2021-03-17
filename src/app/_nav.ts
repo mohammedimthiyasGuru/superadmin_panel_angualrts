2872b2b3d4e1d1e146e7ecae01a5e7c90f811f93
Mohammed Imthiyas <mohammedimthi2395@gmail.com>
2021-03-17 07:14:23 +0530

    commitebase

---
+++ src/app/_nav.ts
@@ -0,0 +1,94 @@
+import { INavData } from '@coreui/angular';
+
+export const navItems: INavData[] = [
+  {
+    name: 'Dashboard',
+    url: '/dashboard',
+    icon: 'icon-speedometer',
+    badge: {
+      variant: 'info',
+      text: 'NEW'
+    }
+  },
+  {
+    title: true,
+    name: 'Admin Menu'
+  },
+  {
+    name: 'Restaurant',
+    url: '/base',
+    icon: 'icon-puzzle',
+    children: [
+      {
+        name: 'Create restaurant',
+        url: '/base/cards',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'List restaurant',
+        url: '/base/cards',
+        icon: 'icon-puzzle'
+      },
+    ]
+  },
+  // {
+  //   name: 'Colors',
+  //   url: '/theme/colors',
+  //   icon: 'icon-drop'
+  // },
+  {
+    title: true,
+    name: 'Components'
+  },
+  {
+    name: 'Base',
+    url: '/base',
+    icon: 'icon-puzzle',
+    children: [
+      {
+        name: 'Cards',
+        url: '/base/cards',
+        icon: 'icon-puzzle'
+      },
+    ]
+  },
+  {
+    name: 'Pages',
+    url: '/pages',
+    icon: 'icon-star',
+    children: [
+      {
+        name: 'Login',
+        url: '/login',
+        icon: 'icon-star'
+      },
+      {
+        name: 'Register',
+        url: '/register',
+        icon: 'icon-star'
+      },
+      {
+        name: 'Error 404',
+        url: '/404',
+        icon: 'icon-star'
+      },
+      {
+        name: 'Error 500',
+        url: '/500',
+        icon: 'icon-star'
+      }
+    ]
+  },
+  // {
+  //   name: 'Disabled',
+  //   url: '/dashboard',
+  //   icon: 'icon-ban',
+  //   badge: {
+  //     variant: 'secondary',
+  //     text: 'NEW'
+  //   },
+  //   attributes: { disabled: true },
+  // },
+
+
+];
